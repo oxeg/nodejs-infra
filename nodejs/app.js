a66936343a675d9366f60e04d41116af4d4e6c24
egor shubin <shubin.egor@gmail.com>
2023-04-17 21:24:19 +0200

    added postgres database

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,10 +1,20 @@
 var express = require('express')
 var app = express()
 
+const { Pool } = require('pg');
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+
 const product = 'Fake Product'
 
 app.get('/', function (req, res) {
     res.send(`Hello ${product}`)
+})
+
+pool.query('SELECT true as value', (err, result) => {
+    if (err) {
+        return console.error('Error connecting to database', err.stack)
+    }
+    console.log(`db connection works: ${result.rows[0].value}`)
 })
 
 app.listen(8080, function () {
