2d84edd43fb8d088ddc043b63d881b8ffe0e1c5b
egor shubin <shubin.egor@gmail.com>
2023-04-17 20:39:00 +0200

    fixed node app output

--- nodejs/app.js
+++ nodejs/app.js
@@ -4,7 +4,7 @@ var app = express()
 const product = 'Fake Product'
 
 app.get('/', function (req, res) {
-    res.send('Hello ${product}')
+    res.send(`Hello ${product}`)
 })
 
 app.listen(8080, function () {
